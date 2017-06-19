f0f0e09e64bcc0faf44a91cfb1c05146d4ba5a98
Deni Zakya <denizakya@gmail.com>
2017-06-19 10:05:50 +0700

    Update libmodbus

--- Vendor/libmodbus/config.h
+++ Vendor/libmodbus/config.h
@@ -141,7 +141,7 @@
 #define PACKAGE_NAME "libmodbus"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libmodbus 3.0.1"
+#define PACKAGE_STRING "libmodbus 3.1.4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libmodbus"
@@ -150,7 +150,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.0.1"
+#define PACKAGE_VERSION "3.1.4"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
@@ -159,7 +159,7 @@
 #define TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define VERSION "3.0.1"
+#define VERSION "3.1.4"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
